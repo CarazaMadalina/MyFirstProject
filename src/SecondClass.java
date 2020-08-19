5f06485c6675bed0c0637f116979dd883d39fb2d
Madalina Caraza <c_mcaraza@groupon.com>
2020-08-19 21:27:21 +0300

    Message

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,6 +1,8 @@
 public class SecondClass {
-    public String car3;
+    public String car0;
     public String car4;
+    public String car8;
+    public String ana;
 
 
     public static void main(String[] args) {
